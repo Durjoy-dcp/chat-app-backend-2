ccf820629cf389675f04bf90dece02e18eba6ae0
Durjoy-dcp <unknownccxv@gmail.com>
2023-03-31 19:57:37 +0600

    login working

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const generateToken = require("../config/generateToken");
 const User = require("../models/userModels");
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -25,10 +26,28 @@ const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
       password: user.password,
       pic: user.pic,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
     throw new Error("Failed to create user");
   }
 });
-module.exports = { registerUser };
+const authUser = asyncHandler(async function (req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Incorrect email & Password");
+  }
+});
+module.exports = { registerUser, authUser };
--- models/userModels.js
+++ models/userModels.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const UserModels = mongoose.Schema(
   {
     name: {
@@ -24,5 +25,15 @@ const UserModels = mongoose.Schema(
     timestamps: true,
   }
 );
+UserModels.methods.matchPassword = async function (givenPassword) {
+  return await bcrypt.compare(givenPassword, this.password);
+};
+UserModels.pre("save", async function (next) {
+  if (!this.isModified()) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 const User = mongoose.model("User", UserModels);
 module.exports = User;
